2caa04dc59b0bbae04e8f326dd9d108499fb923f
redcorjo <jordipromotions@gmail.com>
2018-09-03 23:25:10 +0200

    with annotations working and maven configured

--- src/main/java/com/redcorjo/webapishell/BrokerWebservice.java
+++ src/main/java/com/redcorjo/webapishell/BrokerWebservice.java
@@ -1,12 +1,24 @@
 package com.redcorjo.webapishell;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.UriInfo;
 
 
 @Path("v1")
 public class BrokerWebservice {
+
+    @Context
+    UriInfo uriInfo;
+    @Context
+    Request request;
+    @Context
+    HttpServletRequest req;
 
     @Path("test")
     @GET
